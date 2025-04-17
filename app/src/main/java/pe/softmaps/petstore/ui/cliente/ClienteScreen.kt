f379d020a1a16f36bff592aff1b730279dccbbb6
Marco <marco_x1230@hotmail.com>
2025-04-17 15:20:55 -0500

    Añadiendo BasicSecureTextField para las contraseñas

--- app/src/main/java/pe/softmaps/petstore/ui/cliente/ClienteScreen.kt
+++ app/src/main/java/pe/softmaps/petstore/ui/cliente/ClienteScreen.kt
@@ -1,6 +1,9 @@
 package pe.softmaps.petstore.ui.cliente
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,11 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicSecureTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.TextObfuscationMode
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,7 +38,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,7 +57,7 @@ fun ClienteScreen() {
 @Composable
 fun FormularioLogin() {
     var correo by remember { mutableStateOf("") }
-    var contrasena by remember { mutableStateOf("") }
+    var contrasena = remember { TextFieldState() }
 
     Text(text = "Iniciar Sesión como Cliente")
     Spacer(
@@ -68,19 +76,12 @@ fun FormularioLogin() {
         }
     )
     Spacer(Modifier.height(dimensionResource(R.dimen.small_padding)))
-    CampoTexto(
-        value = contrasena,
-        label = "Contraseña",
-        visualTransformation = PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        leadingIcon = {
-            Icon(
-                Icons.Default.Lock, contentDescription = null
-            )
-        },
-        onValueChange = {
-            contrasena = it
-        })
+
+    CampoTextoContrasena(
+        state = contrasena,
+        modifier = Modifier.padding(dimensionResource(R.dimen.small_padding)),
+    )
+
     Spacer(Modifier.height(dimensionResource(R.dimen.medium_padding)))
     Button(onClick = {}) {
         Text(text = "Iniciar Sesión")
@@ -134,3 +135,64 @@ fun CampoTexto(
             )
     )
 }
+
+@Composable
+fun CampoTextoContrasena(
+    state: TextFieldState,
+    modifier: Modifier = Modifier,
+) {
+    var showPassword by remember { mutableStateOf(false) }
+    BasicSecureTextField(
+        state = state,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.small_padding))
+            .border(
+                1.dp, Color.LightGray, RoundedCornerShape(dimensionResource(R.dimen.small_padding))
+            )
+            .padding(6.dp),
+        textObfuscationMode = if (showPassword) TextObfuscationMode.Visible
+        else TextObfuscationMode.RevealLastTyped,
+        decorator = { innerTextField ->
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                ){
+                    // Icono izquierdo (candado)
+                    Icon( Icons.Filled.Lock,
+                        contentDescription = "Ícono de contraseña",
+                        modifier = Modifier
+                            .size(24.dp)
+                            .padding(end = 8.dp)
+                    )
+                    Box(modifier = Modifier.weight(1f)) {
+                        // Placeholder
+                        if (state.text.isEmpty()) {
+                            Text(
+                                text = "Ingrese Contraseña",
+                                color = Color.Gray
+                            )
+                        }
+                        innerTextField()
+                    }
+
+
+                }
+                // Icono derecho (ojo)
+                Icon(if (showPassword) {
+                    Icons.Filled.Visibility
+                } else {
+                    Icons.Filled.Lock
+                }, contentDescription = "Toggle password visibility",
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .requiredSize(48.dp)
+                        .padding(16.dp)
+                        .clickable { showPassword = !showPassword }
+                )
+            }
+        }
+    )
+}
+
